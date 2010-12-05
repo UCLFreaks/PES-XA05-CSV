80397ca4f21db262ec1f94905dc62ee2f540d6e8
Vratislav Kalenda <v.kalenda@gmail.com>
2010-12-05 19:41:28 +0100

    Testing libraries

--- lib/main.rb
+++ lib/main.rb
@@ -2,3 +2,10 @@
 # and open the template in the editor.
 
 puts "Hello World"
+require "./rubygame/lib/rubygame.rb"
+
+
+Rubygame.init
+screen = Rubygame::Screen.new [1000, 200]
+sleep(10)
+Rubygame.quit  
