7db3d95abb616eb30c1c27d082c2baad2272ef1d
Vratislav Kalenda <v.kalenda@gmail.com>
2010-12-06 13:06:06 +0100

    quick main fix

--- lib/main.rb
+++ lib/main.rb
@@ -8,7 +8,7 @@ units2 = [Soldier.new(-10)]
 army1 = Army.new(units1, SimulationStrategy.new)
 army2 = Army.new(units2, SimulationStrategy.new)
 battle = BattleProgram.new(army1,army2)
-battle.info
+
 
 
 vis = Visualization.new(battle)
