e52be1d1d55ab50db6756533b17387bc0c5f3f1d
Vratislav Kalenda <v.kalenda@gmail.com>
2010-12-17 00:21:56 +0100

    grid preparation

--- lib/main.rb
+++ lib/main.rb
@@ -4,7 +4,7 @@ require './visualization/unit_extensions.rb'
 require './visualization/visualization.rb'
 
 
-units1 = [Tank.new(15),Soldier.new(10),Soldier.new(12),Soldier.new(50),Tank.new(8)]
+units1 = [Tank.new(15),Soldier.new(10),Soldier.new(12),Tank.new(8)]
 units2 = [Soldier.new(-8),Soldier.new(-3),Soldier.new(-1),Sniper.new(-14),Tank.new(-9)]
 army1 = Army.new(units1, SimulationStrategy.new)
 army2 = Army.new(units2, SimulationStrategy.new)
