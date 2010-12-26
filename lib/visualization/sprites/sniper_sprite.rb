5e89633341fe3468303654dfb27eddcd6cf6e27a
Vratislav Kalenda <v.kalenda@gmail.com>
2010-12-26 19:29:59 +0100

    Basic sniper animation implemented.

--- lib/visualization/sprites/sniper_sprite.rb
+++ lib/visualization/sprites/sniper_sprite.rb
@@ -2,8 +2,20 @@
 # and open the template in the editor.
 
 class SniperSprite < UnitSprite
-  def get_image_base_name(state)
-    return "soldier" if state == :dead
+  def get_image_base_name()
     return "sniper"
   end
+
+  def setup_animation
+    @number_of_frames = 8
+    add_animation(:dead, 750,false,8)
+    add_animation(:idle, 1000,false,1)
+    add_animation(:run, 500)
+    set_animation(:idle)
+  end
+
+  def relative_weapon_hardpoint
+    return [62,32]
+  end
+
 end
